06c56b74e9527bcdfc3c81e4f3ba32aaed9c8ba1
David Martin <118901855+DesertZito@users.noreply.github.com>
2025-03-17 10:32:05 +0100

    Archivos de common modulo 2

---
+++ sdis/broker/common/Primitiva.java
@@ -0,0 +1,15 @@
+package sdis.broker.common;
+
+    public enum Primitiva {
+        INFO,
+        XAUTH,
+        ADD2L,
+        ADDED,
+        READL,
+        MEDIA,
+        EMPTY,
+        DELETEL,
+        DELETED,
+        NOTAUTH,
+        ERROR,
+    }
