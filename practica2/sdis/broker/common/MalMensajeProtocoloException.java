13f23b8b47f91d9eade2d32f2d192a191f8a75d4
David Martin <118901855+DesertZito@users.noreply.github.com>
2025-05-17 18:22:17 +0200

    Delete practica2/sdis/broker/common/MalMensajeProtocoloException.java

--- practica2/sdis/broker/common/MalMensajeProtocoloException.java
+++
@@ -1,7 +0,0 @@
-package sdis.broker.common;
-
-public class MalMensajeProtocoloException extends Exception {
-    public MalMensajeProtocoloException() {
-        super("Error en el MensajeProtocolo");
-    }
-}
