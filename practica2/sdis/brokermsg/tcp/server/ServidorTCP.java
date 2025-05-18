ce282360b5ab953d478692a496eb7618f8abd4a0
David Martin <118901855+DesertZito@users.noreply.github.com>
2025-05-18 14:25:42 +0200

    Add files via upload

---
+++ practica2/sdis/brokermsg/tcp/server/ServidorTCP.java
@@ -0,0 +1,49 @@
+package sdis.brokermsg.tcp.server;
+
+import sdis.utils.MultiMap;
+
+public class ServidorTCP {
+    public static int NThreads = 5;   //# hilos del ThreadPool
+    public static int ClientesUsados = 0;
+    public static int pushed = 0;
+    public static int pulled = 0;
+    public static void main(String args[]) {
+        int PUERTO = 3000;  //puerto de servicio
+        MultiMap<String, String>
+                mapa = new MultiMap();
+        java.util.concurrent.ExecutorService exec =
+                java.util.concurrent.Executors.newFixedThreadPool(NThreads);
+        try {
+            java.net.ServerSocket sock = new java.net.ServerSocket(PUERTO);
+            System.err.println("Servidor: WHILE [INICIANDO]");
+            Thread mainServer = new Thread(() -> {
+                try {
+                    while (true) {
+                        java.net.Socket socket = sock.accept();
+                        try {
+                            if(ClientesUsados>=NThreads*2){
+                                System.err.println("Conexion Rechazada maximo numero de clientes");
+                            }else{
+                                sdis.brokermsg.tcp.server.Sirviente serv =
+                                        new sdis.brokermsg.tcp.server.Sirviente(socket, mapa);
+                                exec.execute(serv);
+                                ClientesUsados++;
+                            }
+
+                        } catch (java.io.IOException ioe) {
+                            System.out.println("Servidor: WHILE [ERR ObjectStreams]");
+                        }
+                    }  //fin-while
+                } catch (java.io.IOException ioe) {
+                    System.err.println("Servidor: WHILE [Error.E/S]");
+                } catch (Exception e) {
+                    System.err.println("Servidor: WHILE [Error.execute]");
+                }
+            }, "RUN(WHILE)");  //fin-newThread separado para servidor
+            mainServer.start();
+            System.out.println("Servidor: [CORRIENDO]");
+        } catch (java.io.IOException ioe) {
+            System.out.println("Servidor: [ERR SOCKET]");
+        }
+    }
+}
---
+++ practica2/sdis/brokermsg/tcp/server/Sirviente.java
@@ -0,0 +1,163 @@
+package sdis.brokermsg.tcp.server;
+
+import sdis.brokermsg.tcp.common.*;
+import sdis.utils.MultiMap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Sirviente implements Runnable {
+    private static ConcurrentHashMap<String, String> registroUsuarios = new ConcurrentHashMap<>();
+    private static BlacklistManager managerConexiones = new BlacklistManager(3);
+    private static BlacklistManager managerLogins = new BlacklistManager(2);
+    private final java.net.Socket socket;
+    private final MultiMap<String, String> mapa;
+    private final java.io.ObjectOutputStream oos;
+    private final java.io.ObjectInputStream ois;
+    private final int ns;
+    private static java.util.concurrent.atomic.AtomicInteger nInstance =
+            new java.util.concurrent.atomic.AtomicInteger();
+
+    public Sirviente(java.net.Socket s, MultiMap<String, String> c) throws java.io.IOException {
+        this.socket = s;
+        this.mapa = c;
+        this.ns = nInstance.getAndIncrement();
+        this.oos = new java.io.ObjectOutputStream(socket.getOutputStream());
+        this.ois = new java.io.ObjectInputStream(socket.getInputStream());
+    }  //se invoca en el Servidor, usualmente
+
+    public void run() {
+        String clientIP = socket.getInetAddress().getHostAddress();
+        String userlogged = "NA";
+        try {
+            registroUsuarios.put("cllamas", "qwerty");
+            registroUsuarios.put("hector", "lkjlkj");
+            registroUsuarios.put("sdis", "987123");
+            registroUsuarios.put("admin","$%&/()=");
+            System.out.println(Strings.SERVER_WAITING);
+            boolean fin = false;
+            boolean authored = false;
+            MensajeProtocolo ms;
+
+            if (managerConexiones.isIPBaneada(clientIP)) {
+                ms = new MensajeProtocolo(Primitiva.ERROR, Strings.MAX_CONNECTIONS_REACHED_ERROR);
+                oos.writeObject(ms);
+                fin = true;
+            }else if(managerLogins.isIPBaneada(clientIP)){
+                managerConexiones.registraIntento(clientIP);
+                ms = new MensajeProtocolo(Primitiva.ERROR, Strings.MAX_LOGIN_ATTEMPTS_REACHED_ERROR);
+                oos.writeObject(ms);
+                fin = true;
+            }
+            else{
+                managerConexiones.registraIntento(clientIP);
+                System.out.println("[BM] (connections) for " +  clientIP + " = " + managerConexiones.getIntentos(clientIP));
+                ms = new MensajeProtocolo(Primitiva.INFO, Strings.WELCOME_MESSAGE);
+                oos.writeObject(ms);
+            }
+
+            while (!fin) {
+                String mensaje;  //String multipurpose
+                MensajeProtocolo me = (MensajeProtocolo) ois.readObject();
+                // me y ms: mensajes entrante y saliente
+                System.out.println("Sirviente: " + ns +" from: "+ clientIP + ": [ME: " + me); // depuracion me
+                switch (me.getPrimitiva()) {
+                    case XAUTH:
+                        String username = me.getIdCola();
+                        String password = me.getMensaje();
+                        if (registroUsuarios.containsKey(username) && registroUsuarios.get(username).equals(password)) {//Si el usuario y contraseña están en el multimap
+                            ms = new MensajeProtocolo(Primitiva.XAUTH, Strings.USER_LOGGED_SUCCESSFULLY);
+                            authored = true;
+                            userlogged = username;
+                            oos.writeObject(ms);
+                        } else {
+                            managerLogins.registraIntento(clientIP);
+                            if (managerLogins.isIPBaneada(clientIP)) {
+                                ms = new MensajeProtocolo(Primitiva.ERROR, Strings.MAX_LOGIN_ATTEMPTS_REACHED_ERROR);
+                                oos.writeObject(ms);
+                                fin =  true;
+                            } else {
+                                ms = new MensajeProtocolo(Primitiva.NOTAUTH, Strings.LOGIN_ERROR);
+                                System.out.println("[BM] (login fails) for " +  clientIP + " = " + managerLogins.getIntentos(clientIP));
+                                oos.writeObject(ms);
+                            }
+                        }
+                        break;
+                    case READQ:
+                        mensaje = mapa.pop(me.getIdCola());
+                        if (null != mensaje) {
+                            ms = new MensajeProtocolo(Primitiva.MSG, mensaje);
+                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
+                            ServidorTCP.pulled++;
+                            oos.writeObject(ms);
+                        } else {
+                            ms = new MensajeProtocolo(Primitiva.EMPTY);
+                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
+                            oos.writeObject(ms);
+                        }
+                        break;
+
+                    case STATE:
+                        if(authored && userlogged.equals("admin")){
+                            ms = new MensajeProtocolo(Primitiva.STATE, "Hilos maximos: " + ServidorTCP.NThreads + " Hilos ocupados: " + ServidorTCP.ClientesUsados + " Mensajes enviados: " + ServidorTCP.pushed + " Mensajes recibidos: " + ServidorTCP.pulled);
+                            oos.writeObject(ms);
+                        }else{
+                            ms = new MensajeProtocolo(Primitiva.NOTAUTH, Strings.NO_ADMIN);
+                            oos.writeObject(ms);
+                        }
+                        break;
+                    case DELETEQ:
+                        if(authored){
+                            if (null == (mensaje = mapa.pop(me.getIdCola()))) {
+                                ms = new MensajeProtocolo(Primitiva.EMPTY);
+                                System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
+                                oos.writeObject(ms);
+                            } else {
+                                while(mensaje!= null){
+                                    mensaje = mapa.pop(me.getIdCola());
+                                }
+                                ms = new MensajeProtocolo(Primitiva.DELETED);
+                                System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
+                                oos.writeObject(ms);
+                            }
+                        }
+                        else{
+                            ms = new MensajeProtocolo(Primitiva.NOTAUTH, Strings.LOGIN_REQUIRED);
+                            oos.writeObject(ms);
+                        }
+                        break;
+                    case ADDMSG:
+                        if (authored){
+                            mapa.push(me.getIdCola(), me.getMensaje());
+                            synchronized (mapa) {
+                                mapa.notify();
+                            }  //despierta un sirviente esperando en un bloqueo de "mapa"
+                            ms = new MensajeProtocolo(Primitiva.ADDED);
+                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
+                            ServidorTCP.pushed++;
+                            oos.writeObject(ms);
+                        }
+                        else{
+                            ms = new MensajeProtocolo(Primitiva.NOTAUTH, Strings.LOGIN_REQUIRED);
+                            oos.writeObject(ms);
+                        }
+                        break;
+                    default:
+                        ms = new MensajeProtocolo(Primitiva.BADCODE, Strings.UNEXPECTTED_ERROR);
+                        oos.writeObject(ms);
+
+
+                }  //fin del selector segun el mensaje entrante
+
+
+            }
+        }catch(Exception io){
+
+        }
+        managerConexiones.clientedesconectado(clientIP);
+        System.out.println("Cliente desconectado. Usuario asociado = " + userlogged);
+        System.out.println("[BM] (connections) for " +  clientIP + " = " + managerConexiones.getIntentos(clientIP));
+        ServidorTCP.ClientesUsados--;
+
+
+    }
+}
